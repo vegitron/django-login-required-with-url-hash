32e389dd800ac0ded816604e9de1552db6104782
Patrick Michaud <pmichaud@uw.edu>
2013-12-02 10:51:01 -0800

    Adding a setup.py, so this can be installed as an egg

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='LoginRequired-HashURL',
+      version='1.0',
+      description='Allows you to require authentication on Django pages that use URL hashes for state.',
+     )
